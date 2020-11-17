e9ea5d7bed22dcb21e07806c106faa7a89348ae2
Yaya <yaya@YayadeMacBook-Pro.local>
2020-11-17 10:51:57 -0600

    changes

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-const car = document.getElementById("car");
-const fire = document.getElementById("fire");
-const score = document.getElementById("score");
+const car = document.getElementById('car');
+const fire = document.getElementById('fire');
+const score = document.getElementById('score');
+
+
+// class Fire {
+//   constructor(x=100, y=) {
+//     this.width = 200;
+//     this.height = 200;
+//     this.x = x;
+//     this.y = y;
+//   }
+// }
 
 // make jump animation
 function jump() {
-  car.classList.add("jump-animation");
-  setTimeout(() =>
-    car.classList.remove("jump-animation"), 500);
+  car.classList.add('jump-animation');
+  setTimeout(() => car.classList.remove('jump-animation'), 500);
 }
 
 document.addEventListener('keypress', (event) => {
   if (!car.classList.contains('jump-animation')) {
     jump();
   }
-})
+});
 
 // get car and fire's data
 setInterval(() => {
-  const carTop = parseInt(window.getComputedStyle(car)
-    .getPropertyValue('top'));
-  const fireLeft = parseInt(window.getComputedStyle(fire)
-    .getPropertyValue('left'));
+  const carTop = parseInt(window.getComputedStyle(car).getPropertyValue('top'));
+  const fireLeft = parseInt(
+    window.getComputedStyle(fire).getPropertyValue('left')
+  );
   score.innerText++;
 
   //fire shows up
   if (fireLeft < 0) {
     fire.style.display = 'none';
   } else {
-    fire.style.display = ''
+    fire.style.display = '';
   }
 
   //define when is losing
   if (fireLeft < 150 && fireLeft > 0 && carTop > 150) {
-    alert("You got a score of: " + score.innerText +
-      "\n\nPlay again?");
-    location.reload();
+    console.log('You got a score of: ' + score.innerText + '\n\nPlay again?');
+    //location.reload();
   }
 }, 50);
